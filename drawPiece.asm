1aeab85153b5c985f094541acbc809edba8f4254
Aly-Abdel-Motaleb <94588581+Aly-Abdel-Motaleb@users.noreply.github.com>
2022-12-08 21:17:44 +0200

    Update drawPiece.asm
    Drawing the Chess Board

--- drawPiece.asm
+++ drawPiece.asm
@@ -1,8 +1,9 @@
 
 ;PUBLIC drawCol,drawRow
 ;EXTRN drawSingleCell2:FAR
-.MODEL COMPACT
+.MODEL Huge
 .286
+
 
 .STACK 64
 .DATA
@@ -243,6 +244,20 @@ BPAWN DB'*', '*', '*','*', '*', '*', '*', '*','*', '*', '*', 00h, 00h, 00h, 00h,
   DB '*','*', '*', '*', '*', '*', 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh,'$' 
   DB '*','*', '*', '*', '*', '*', 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh, 0fh,'$' 
   DB '*', '*', '*', '*', '*', '*','*', 0fh, 0fh, 0fh, 0fh, 0fh, '*', '*', '*', '*', '*', 0fh, 0fh, 0fh, 0fh, 0fh, '&'
+
+
+; Color Matrix
+Board DB 7,8,7,8,7,8,7,8
+      DB 8,7,8,7,8,7,8,7
+      DB 7,8,7,8,7,8,7,8
+      DB 8,7,8,7,8,7,8,7
+      DB 7,8,7,8,7,8,7,8
+      DB 8,7,8,7,8,7,8,7
+      DB 7,8,7,8,7,8,7,8
+      DB 8,7,8,7,8,7,8,7
+
+
+
 wide     equ 40
 height   equ 20
 nextline equ 320
@@ -253,130 +268,145 @@ drawCol db 6
 ; '&' means end of shape
 
 .code 
-DrawPiece    PROC FAR
-;swich to graphics mode
-mov ah,0
-mov al,13h
-int 10h  
-; bx contain row number
-; ax contain Column number
-; dx contains cell first color
-; si contains second color height
-MOV AX,@DATA
-MOV DS,AX  
-;MOV drawCol,4
-;MOV drawRow,5
-MOV MULBYC,0
-MOV MULBYR,0
-MOV BL,drawRow
+; DrawPiece    PROC FAR
+; ;swich to graphics mode
+; mov ah,0
+; mov al,13h
+; int 10h  
+; ; bx contain row number
+; ; ax contain Column number
+; ; dx contains cell first color
+; ; si contains second color height
+; MOV AX,@DATA
+; MOV DS,AX  
+; ;MOV drawCol,4
+; ;MOV drawRow,5
+; MOV MULBYC,0
+; MOV MULBYR,0
+; MOV BL,drawRow
 
-CALL FAR PTR drawSingleCell
+; CALL FAR PTR drawSingleCell
 
-;n:
-;jmp n
-; di column number of cell
-; bx row number of cell 
-MOV AX,@DATA
-MOV DS,AX  
-mov bh,0
-mov bl,drawCol
-mov di,bx
-mov bh,0
-mov Bl,drawRow
-CALL BY40
-CALL BY20
+; ;n:
+; ;jmp n
+; ; di column number of cell
+; ; bx row number of cell 
+; MOV AX,@DATA
+; MOV DS,AX  
+; mov bh,0
+; mov bl,drawCol
+; mov di,bx
+; mov bh,0
+; mov Bl,drawRow
+; CALL BY40
+; CALL BY20
 
-mov   SI,OFFSET WROOK
-loop_:
-mov cl,[si]    
-cmp cl,'*'
-je curr_pix
-mov cx,di ;Column
-mov dx,bx ;Row
-mov al,[si] ;Pixel color
-mov ah,0ch ;Draw Pixel Command
-int 10h    
-jmp not_pix
-curr_pix:  
-mov cx,di ;Column
-mov dx,bx ;Row
-mov bh,0 
-mov ah,0dh ;Draw Pixel Command
-int 10h    
+; mov   SI,OFFSET WROOK
+; loop_:
+; mov cl,[si]    
+; cmp cl,'*'
+; je curr_pix
+; mov cx,di ;Column
+; mov dx,bx ;Row
+; mov al,[si] ;Pixel color
+; mov ah,0ch ;Draw Pixel Command
+; int 10h    
+; jmp not_pix
+; curr_pix:  
+; mov cx,di ;Column
+; mov dx,bx ;Row
+; mov bh,0 
+; mov ah,0dh ;Draw Pixel Command
+; int 10h    
 
-mov cx,di ;Column
-mov dx,bx ;Row
-mov ah,0ch ;Draw Pixel Command
-int 10h    
-not_pix:
-inc si   
-mov cl,[si]    
-cmp cl,'&'
-je exit
-mov cl,[si]  
-cmp cl,'$'
-je inc_row
+; mov cx,di ;Column
+; mov dx,bx ;Row
+; mov ah,0ch ;Draw Pixel Command
+; int 10h    
+; not_pix:
+; inc si   
+; mov cl,[si]    
+; cmp cl,'&'
+; je exit
+; mov cl,[si]  
+; cmp cl,'$'
+; je inc_row
 
-jmp not_row
-inc_row: 
-inc bx
-mov di,MULBYC
-dec di
-inc si    
-not_row:
-inc di
-jmp loop_
-exit:
-JMP O
-DrawPiece ENDP
-BY40 PROC 
-MOV CL,39
-MOV MULBYC,DI
-LBY40:
-ADD MULBYC,DI
-DEC CL
-JNZ LBY40
-ADD MULBYC,5
-MOV DI,MULBYC
-ret
-BY40 ENDP
+; jmp not_row
+; inc_row: 
+; inc bx
+; mov di,MULBYC
+; dec di
+; inc si    
+; not_row:
+; inc di
+; jmp loop_
+; exit:
+; JMP O
+; DrawPiece ENDP
+; BY40 PROC 
+; MOV CL,39
+; MOV MULBYC,DI
+; LBY40:
+; ADD MULBYC,DI
+; DEC CL
+; JNZ LBY40
+; ADD MULBYC,5
+; MOV DI,MULBYC
+; ret
+; BY40 ENDP
 
-BY20 PROC 
-MOV CL,19
-MOV MULBYR,BX
-LBY20:
-ADD MULBYR,BX
-DEC CL
-JNZ LBY20
-INC MULBYR
-MOV BX,MULBYR
-ret
-BY20 ENDP
-drawSingleCell proc FAR
-
-                   mov  ax,0A000h
-                   mov  es,ax
-
-                  ; mov  ah,0
-                  ; mov  al,13h
-                  ; int  10h
+; BY20 PROC 
+; MOV CL,19
+; MOV MULBYR,BX
+; LBY20:
+; ADD MULBYR,BX
+; DEC CL
+; JNZ LBY20
+; INC MULBYR
+; MOV BX,MULBYR
+; ret
+; BY20 ENDP
 
 
-    ; bx contain row number
-    ; ax contain Column number
-    ; dx contains first color
-    ; si contains second color height
-   
 
-    ; width * column + height * 320 * row
-                   
-                   mov bh,0
-                   mov bl,drawCol
-                   mov  ax,bx
-                   mov bh,0
-                   mov  bl,drawRow
-                   mov  dx,3
-                   ;mov si,10
+ChessBoard proc FAR
+                  MOV AX,@DATA
+                  mov ds, ax
+                  mov  ax,0A000h
+                  mov  es,ax
+                  mov  ah,0
+                  mov  al,13h
+                  int  10h
+
+pusha 
+
+
+
+mov ch,8
+xor dx,dx
+mov bx,0
+lea di ,Board
+mov dx,[di]
+mov si,0
+mov ax,0
+
+
+
+
+singleRow:
+;! you have to remove the procedure and put the call here
+;! I have tried but there is a error in th calling 
+
+;! start of proc call
+pusha
+                  ; mov bh,0
+                  ; mov bl,drawCol
+                  ; mov  ax,bx
+                  ; mov bh,0
+                  ; mov  bl,drawRow
+                  ; mov  dx,3
+                  ;mov si,10
     ;oushing dx to save color
                    push dx
     ; computing the equtation
@@ -396,13 +426,12 @@ drawSingleCell proc FAR
                    mov  cx,wide
                    mov al,0ch
     ; put color on al
-                   
-                     ; draw one line of the box with cool down color 
-                     cmp si,0
-                     je nored
+                    ; draw one line of the box with cool down color 
+                    cmp si,0
+                    je nored
     looping2:       
-                   rep  stosb
-                   dec  si
+                  rep  stosb
+                  dec  si
 
 
     start1:         
@@ -429,11 +458,117 @@ drawSingleCell proc FAR
     ;moving to the next line
                    add  di,nextline
     ;assign the width again
-                   mov  cx,wide
+                  mov  cx,wide
     ; repeat till the height become 0
-                   cmp  bx,0
-                   jne  looping1
-                   ret
+                  cmp  bx,0
+                  jne  looping1
+popa
+;! end of the calling
+
+inc di
+inc AX
+dec ch
+mov dx,[di]
+cmp ch,0
+jne singleRow
+je  incRow 
+
+
+
+incRow:
+  inc bx
+  cmp bx,8
+  mov ch,8
+  mov ax,0
+  jne singleRow
+popa
+
+
+ChessBoard ENDP
+
+
+drawSingleCell proc FAR
+
+
+
+
+                  ; mov  ah,0
+                  ; mov  al,13h
+                  ; int  10h
+    ; bx contain row number
+    ; ax contain Column number
+    ; dx contains first color
+    ; si contains second color height
+
+    ; width * column + height * 320 * row
+                  
+    ;               mov bh,0
+    ;               mov bl,drawCol
+    ;               mov  ax,bx
+    ;               mov bh,0
+    ;               mov  bl,drawRow
+    ;               mov  dx,3
+    ;               ;mov si,10
+    ; ;oushing dx to save color
+    ;                push dx
+    ; ; computing the equtation
+    ;                mov  cx ,wide
+    ;                mul  cx
+    ;                xchg ax,bx
+    ;                mov  cx, height
+    ;                mul  cx
+    ;                mov  cx,320
+    ;                mul  cx
+    ;                add  ax,bx
+    ;                mov  di, ax
+    ;                pop  dx
+
+    ;                mov  bx,height
+    ;                sub bx,si
+    ;                mov  cx,wide
+    ;                mov al,0ch
+    ; ; put color on al
+                   
+    ;                  ; draw one line of the box with cool down color 
+    ;                  cmp si,0
+    ;                  je nored
+    ; looping2:       
+    ;                rep  stosb
+    ;                dec  si
+
+
+    ; start1:         
+    ; ;return to the begining of the line
+    ;                sub  di,wide
+    ; ;moving to the next line
+    ;                add  di,nextline
+    ; ;assign the width again
+    ;                mov  cx,wide
+    ; ; repeat till the height become 0
+    ;                cmp  si,0
+    ;                jne  looping2
+    ; nored:
+    ; ; draw one line of the box
+    ; mov  al,dl
+    ; looping1:       
+    ;                rep  stosb
+    ;                dec  bx
+
+
+    ; start2:         
+    ; ;return to the begining of the line
+    ;                sub  di,wide
+    ; ;moving to the next line
+    ;                add  di,nextline
+    ; ;assign the width again
+    ;                mov  cx,wide
+    ; ; repeat till the height become 0
+    ;                cmp  bx,0
+    ;                jne  looping1
+    ;                ret
 drawSingleCell endp
+
 O:
-END DrawPiece
+; END DrawPiece
+
+End ChessBoard
