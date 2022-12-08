bee83d5a6db75ec6d5dc262e63c6949100626b32
omar said <87082462+Omar-Said-4@users.noreply.github.com>
2022-12-09 00:41:54 +0200

    Add files via upload

--- MainGame.asm
+++ MainGame.asm
@@ -393,7 +393,7 @@ cmp al , 'R'
 jnz not_white_rook
 mov curr_draw,offset WROOK
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_white_rook:
 cmp al , 'r'
 jnz not_black_rook
@@ -404,7 +404,7 @@ cmp al , 'h'
 jnz not_black_knight
 mov curr_draw,offset BKNIGHT
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_black_knight:
 cmp al , 'H'
 jnz not_white_knight
@@ -415,49 +415,49 @@ cmp al , 'p'
 jnz not_black_pawn
 mov curr_draw,offset BPAWN
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_black_pawn:
 cmp al , 'P'
 jnz not_white_pawn
 mov curr_draw,offset WPAWN
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_white_pawn:
 cmp al , 'k'
 jnz not_black_king
 mov curr_draw,offset BKING
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_black_king:
 cmp al , 'K'
 jnz not_white_king
 mov curr_draw,offset WKING
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_white_king:
 cmp al , 'q'
 jnz not_black_queen
 mov curr_draw,offset BQUEEN
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_black_queen:
 cmp al , 'Q'
 jnz not_white_queen
 mov curr_draw,offset WQUEEN
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_white_queen:
 cmp al , 'b'
 jnz not_black_bishop
 mov curr_draw,offset BBISHOP
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_black_bishop:
 cmp al , 'B'
 jnz not_white_bishop
 mov curr_draw,offset WBISHOP
 CALL FAR PTR DrawPiece
-jmp far ptr nextPiece
+jmp  nextPiece
 not_white_bishop:
 nextPiece:
 pop si
@@ -468,13 +468,13 @@ inc drawCol
 dec ch
 cmp ch,0
 je exiti
-jmp far ptr inner_pieces_draw
+jmp  inner_pieces_draw
 exiti:
 inc drawRow
 dec cl
 cmp cl,00h
 je done
-jmp far ptr outer_pieces_draw
+jmp  outer_pieces_draw
 done:
 
 
